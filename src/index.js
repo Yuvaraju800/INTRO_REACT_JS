60996d30cbdac8456f72db09c4409ceeb9bb8bb0
Yuvaraju800 <127011253+Yuvaraju800@users.noreply.github.com>
2023-09-25 22:46:24 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var react = require("react");
+var reactDOM = require("react-dom");
+
+reactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>My name is yuvi</p>
+  </div>,
+  document.getElementById("root")
+);
